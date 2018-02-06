0b0e09fcf56b2ae7cec9bead91c3ad308729241b
doniatung <doniatung32@gmail.com>
2018-02-05 21:30:40 -0500

    wahooo

--- clickdraw.js
+++ clickdraw.js
@@ -1,57 +1,54 @@
 //Donia Tung
 //SoftDev2 pd7
-//2018-02-03
+//2018-02-05
 
 //retrieve node in DOM via ID
 var c = document.getElementById("slate");
-  //instantiate a CanvasRenderingContext2D object
+//instantiate a CanvasRenderingContext2D object
 var ctx = c.getContext("2d");
+//declare variables
+var shape = 0;
+var x, y;
+var oldx;
+var oldy;
 
-var q = 0;
-
-  var circle = function(e){
-    var x = e.offsetX;
-    var y = e.offsetY;
-    ctx.beginPath()
-    ctx.arc(x,y, 10, 0, 2*Math.PI)
-    ctx.stroke()
-  }
-
-var rect = function(e){
-  var x = e.offsetX;
-  var y = e.offsetY;
-  ctx.fillRect(x,y, 10, 10)
-}
-
-
-
+//draw function
 var draw = function(e){
-  var x = e.offsetX;
-  var y = e.offsetY;
-  if (q == 0){
-    ctx.beginPath()
-    ctx.arc(x,y, 10, 0, 2*Math.PI)
-    ctx.stroke()
+  e.preventDefault();
+  x = e.offsetX;
+  y = e.offsetY;
+  if (shape == 0){
+    ctx.beginPath();
+    ctx.arc(x,y, 10, 0, 2*Math.PI);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.moveTo(oldx, oldy);
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    oldx = x;
+    oldy = y;
   }
   else{
-    ctx.fillRect(x,y, 10, 10)
+    ctx.fillRect(x,y, 30, 30);
   }
 }
+
+//adding event listener to the canvas
 c.addEventListener("click", draw);
-
+//toggle function
 document.getElementById('toggle').onclick = function(){
-  if (q == 0){
-    q = 1;
+  if (shape == 0){
+    shape = 1;
   }
   else{
-    q = 0;
+    shape = 0;
   }
 }
-
+//clear function
 var erase = function(e){
   e.preventDefault();
   ctx.clearRect(0, 0, c.width, c.height)
 }
-
+//adding event listener to the clear button
 var clear = document.getElementById("clear")
 clear.addEventListener("click", erase);
